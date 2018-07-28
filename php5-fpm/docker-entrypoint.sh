a49a474e21abada6d22c19d39c30dbc6cebf13e8
Parinya Chaipetch <parinya.siamdevs@gmail.com>
2018-07-28 14:52:18 +0700

    Build php.ini.2 to php.ini with env

--- php5-fpm/docker-entrypoint.sh
+++ php5-fpm/docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 set -eo pipefail
-shopt -s nullglob
-#env | j2 --format=env /var/templates/php.ini.j2 /usr/local/etc/php/php.ini
+env | j2 --format=env /var/templates/php.ini.j2 > /usr/local/etc/php/php.ini
 exec "$@"
